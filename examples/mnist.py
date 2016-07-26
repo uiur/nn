037bb94d32974ad5a5f12717dd213b504e4a3782
Kazato Sugimoto <uiureo@gmail.com>
2016-07-26 14:18:59 +0900

    Implement cross entropy

--- examples/mnist.py
+++ examples/mnist.py
@@ -10,7 +10,7 @@ net = Network([
     Input(784),
     Dense(30),
     Dense(10),
-])
+], loss=CrossEntropy())
 
 (training_data, validation_data, test_data) = mnist_loader.load()
 X_train, y_train = training_data
@@ -28,7 +28,7 @@ batch_size = 30
 for epoch in range(100):
     for i in range(len(X_train) // batch_size):
         X_batch, y_batch = util.batch(X_train, y_train, batch_size=batch_size)
-        net.train_on_batch(X_batch, y_batch, learning_rate=3.0)
+        net.train_on_batch(X_batch, y_batch, learning_rate=1.)
 
     train_accuracy, train_loss = evaluate(net, X_batch, y_batch)
     valid_accuracy, valid_loss = evaluate(net, X_valid, y_valid)
--- network.py
+++ network.py
@@ -1,9 +1,32 @@
 import numpy as np
 
+class MeanSquaredError():
+    def __init__(self):
+        pass
+
+    def nabla(self, a, y):
+        return a - y
+
+    def call(self, a, y):
+        return 0.5 * np.mean(np.linalg.norm(y - a, axis=1) ** 2)
+
+class CrossEntropy():
+    def __init__(self):
+        pass
+
+    def nabla(self, a, y):
+        return -1 * (y - a) / (a * (1 - a))
+
+    def call(self, a, y):
+        return -np.mean(y * np.log(a) + (1 - y) * np.log(1 - a))
+
+
 class Network():
-    def __init__(self, layers):
+    def __init__(self, layers, loss=MeanSquaredError()):
         self.layers = layers
         self.layer_nums = [layer.output_num() for layer in self.layers]
+        self.loss_function = loss
+
         for l, layer in enumerate(self.layers):
             if l - 1 < 0:
                 continue
@@ -27,14 +50,11 @@ class Network():
 
     # mean squared error
     def loss(self, X, y):
-        outputs = self.output(X)
-        return 0.5 * np.mean(np.linalg.norm(y - outputs, axis=1) ** 2)
+        activation = self.output(X)
+        return self.loss_function.call(activation, y)
 
     def output(self, X):
         return np.array([self.feedforward(x)[-1] for x in X])
-
-    def nabla_loss(self, a, y):
-        return a - y
 
     def train_on_batch(self, X, y, learning_rate=0.1):
         batch_num = len(X)
@@ -64,8 +84,8 @@ class Network():
             activations = self.feedforward(x)
 
             last_layer = self.layers[-1]
-            nabla = self.nabla_loss(last_layer.activation, y[i])
-            last_layer.error = nabla * sigmoid_prime(last_layer.weighted_input)
+            nabla = self.loss_function.nabla(last_layer.activation, y[i])
+            last_layer.error = nabla * last_layer.activation_function.prime(last_layer.weighted_input)
 
             for l in range(len(self.layers)-1, 1, -1):
                 layer = self.layers[l]
@@ -87,17 +107,26 @@ class Network():
 
 
 
-def sigmoid(z):
-    return 1.0 / (1.0 + np.exp(-z))
+class Sigmoid():
+    def call(self, z):
+        return 1.0 / (1.0 + np.exp(-z))
+
+    def prime(self, z):
+        return self.call(z) * (1 - self.call(z))
 
 
-def sigmoid_prime(z):
-    return sigmoid(z) * (1 - sigmoid(z))
+class Softmax():
+    def call(self, z):
+        return np.exp(z) / np.sum(np.exp(z))
+
+    def prime(self, z):
+        return self.call(z) * (1 - self.call(z))
 
 
 class Dense():
-    def __init__(self, n):
+    def __init__(self, n, activation=Sigmoid()):
         self.n = n
+        self.activation_function = activation
 
     def build(self, input_num):
         self.weight = np.random.randn(self.n, input_num)
@@ -106,7 +135,7 @@ class Dense():
 
     def call(self, x):
         weighted_input = np.dot(self.weight, x) + self.bias
-        activation = sigmoid(weighted_input)
+        activation = self.activation_function.call(weighted_input)
 
         self.weighted_input = weighted_input
         self.activation = activation
@@ -115,7 +144,7 @@ class Dense():
 
     # returns errors of a previous layer
     def backprop(self, prev_layer):
-        return np.dot(self.weight.T, self.error) * sigmoid_prime(prev_layer.weighted_input)
+        return np.dot(self.weight.T, self.error) * self.activation_function.prime(prev_layer.weighted_input)
 
     def nabla(self, prev_layer):
         w_nabla = np.array([prev_layer.activation] * self.n) * np.array([self.error] * self.input_num).T
